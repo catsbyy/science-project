07da7d52c45c1fbf0a4dd79bd9bda35f99e93d5c
v-andrusenko <andrusenko.vp1@gmail.com>
2023-02-11 17:26:38 +0200

    update

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const cors = require("cors");
 const { get } = require("./routes/studentRouter");
 const PORT = process.env.PORT || 3001;
 const app = express();
-//app.use(express.static("public"));
 
 app.use(cors());
 
@@ -54,12 +53,11 @@ app.get("/server", async (req, res) => {
 
 app.get("/get-results", async (req, res) => {
   console.log(req.query);
-  let studentsSql = `SELECT * FROM student_details`;
-  //getResultsByFilters(req.query);
+  const students = await getResultsByFilters(req.query);
+  //`SELECT * FROM student_details`;
+
   // функция возвращает готовый список студентов, поэтому удали sql запрос ниже!
 
-  console.log(studentsSql);
-  const students = await connectionPromise(studentsSql, "");
   res.json({
     students: students,
   });
@@ -73,13 +71,6 @@ app.get("/get-student-details/:id", async (req, res) => {
     student: student,
   });
 });
-
-// отримання усіх кандидатів
-// connection.query("SELECT * FROM student_details", function (err, results, fields) {
-//   console.log(err);
-//   console.log(results); // самі дані
-//   //console.log(fields);  мета-дані полів
-// });
 
 // закриття підключення
 // connection.end(function (err) {
@@ -110,101 +101,105 @@ app.use(function (req, res, next) {
 });
 
 const getResultsByFilters = async function (params) {
-  let studentsSql = "";
-  if (Object.keys(params).length === 0) studentsSql = `SELECT * FROM student_details`;
+  let students = [];
+  const defaultValue = await connectionPromise(`SELECT * FROM student_details`, "");
+  if (Object.keys(params).length === 0) students = defaultValue;
   else {
-    //studentsSql = formSql(params);
-    const techAndToolsIds = params.studentTechAndTools
-      .split(";")
-      .filter(function (el) {
-        return el != "";
-      })
-      .map(Number);
+    let techAndToolsIds = "";
+    
+    if (params.studentTechAndTools !== "" && params.studentTechAndTools !== null && params.studentTechAndTools !== undefined) {
+      techAndToolsIds = params.studentTechAndTools
+        .split(";")
+        .filter(function (el) {
+          return el != "";
+        })
+        .map(Number);
+    }
 
+    // основні параметри
     // співпадіння по посаді
-    let positionMatches;
-    if (params.studentPosition !== "" || params.studentPosition !== null)
-      positionMatches = await connectionPromise(
-        `SELECT * FROM student_details WHERE student_details.position_id = "${params.studentPosition}"`,
-        ""
-      );
+    let positionMatches = getMatchesByFilter(
+      params.studentPosition,
+      `SELECT * FROM student_details WHERE student_details.position_id = "${params.studentPosition}"`
+    );
 
     // співпадіння по області роботи
-    let workAreaMatches;
-    if (params.studentWorkArea !== "" || params.studentWorkArea !== null)
-      workAreaMatches = await connectionPromise(
-        `SELECT * FROM student_details WHERE student_details.work_area_id = "${params.studentWorkArea}"`,
-        ""
-      );
+    let workAreaMatches = getMatchesByFilter(
+      params.studentWorkArea,
+      `SELECT * FROM student_details WHERE student_details.work_area_id = "${params.studentWorkArea}"`
+    );
+
     // співпадіння по досвіду роботи
-    let workExpMatches;
-    if (params.studentWorkExp !== "" || params.studentWorkExp !== null)
-      workExpMatches = await connectionPromise(
-        `SELECT * FROM student_details WHERE student_details.work_experience_id = "${params.studentWorkExp}"`,
-        ""
-      );
-    // співпадіння по технологіям
-    let techAndToolsMatches;
-    if (params.techAndToolsIds !== "" || params.techAndToolsIds !== null)
-      techAndToolsMatches = await connectionPromise(
-        `SELECT * FROM student_details WHERE student_details.technologies_and_tools = "${params.techAndToolsIds}"`,
-        ""
-      );
+    let workExpMatches = getMatchesByFilter(
+      params.studentWorkExp,
+      `SELECT * FROM student_details WHERE student_details.work_experience_id = "${params.studentWorkExp}"`
+    );
+
+    // співпадіння по технологіям - найголовніше
+    let techAndToolsMatches = getMatchesByFilter(
+      techAndToolsIds,
+      `SELECT * FROM student_details WHERE student_details.technologies_and_tools = "${techAndToolsIds}"`
+    );
+
     // співпадіння по англійській
-    let englishMatches;
-    if (params.studentEnglish !== "" || params.studentEnglish !== null)
-      englishMatches = await connectionPromise(
-        `SELECT * FROM student_details WHERE student_details.english_level_id = "${params.studentEnglish}"`,
-        ""
-      );
+    let englishMatches = getMatchesByFilter(
+      params.studentEnglish,
+      `SELECT * FROM student_details WHERE student_details.english_level_id = "${params.studentEnglish}"`
+    );
+
     // співпадіння по освіті
-    let educationMatches;
-    if (params.studentEducation !== "" || params.studentEducation !== null)
-      educationMatches = await connectionPromise(
-        `SELECT * FROM student_details WHERE student_details.education_level_id = "${params.studentEducation}"`,
-        ""
-      );
+    let educationMatches = getMatchesByFilter(
+      params.studentEducation,
+      `SELECT * FROM student_details WHERE student_details.education_level_id = "${params.studentEducation}"`
+    );
+
+    // додаткові параметри
     // співпадіння по області
-    let regionMatches;
-    if (params.studentRegion !== "" || params.studentRegion !== null)
-      regionMatches = await connectionPromise(
-        `SELECT * FROM student_details WHERE student_details.region_id = "${params.studentRegion}"`,
-        ""
-      );
+    let regionMatches = getMatchesByFilter(
+      params.studentRegion,
+      `SELECT * FROM student_details WHERE student_details.region_id = "${params.studentRegion}"`
+    );
+
     // співпадіння по місту
-    let cityMatches;
-    if (params.studentCity !== "" || params.studentCity !== null)
-      cityMatches = await connectionPromise(
-        `SELECT * FROM student_details WHERE student_details.city = "${params.studentCity}"`,
-        ""
-      );
+    let cityMatches = getMatchesByFilter(
+      params.studentCity,
+      `SELECT * FROM student_details WHERE student_details.city = "${params.studentCity}"`
+    );
+
     // співпадіння по місцю роботи
-    let workplaceMatches;
-    if (params.studentWorkplace !== "" || params.studentWorkplace !== null)
-      workplaceMatches = await connectionPromise(
-        `SELECT * FROM student_details WHERE student_details.workplace_id = "${params.studentWorkplace}"`,
-        ""
-      );
+    let workplaceMatches = getMatchesByFilter(
+      params.studentWorkplace,
+      `SELECT * FROM student_details WHERE student_details.workplace_id = "${params.studentWorkplace}"`
+    );
+
     // співпадіння по заробітній платі
-    let salaryMatches;
-    if (params.studentSalary !== "" || params.studentSalary !== null)
-      salaryMatches = await connectionPromise(
-        `SELECT * FROM student_details WHERE student_details.salary_id = "${params.studentSalary}"`,
-        ""
-      );
+    let salaryMatches = getMatchesByFilter(
+      params.studentSalary,
+      `SELECT * FROM student_details WHERE student_details.salary_id = "${params.studentSalary}"`
+    );
 
-    //`SELECT * FROM student_details WHERE student_details.position = "${params.studentPosition}"`;
+    // перетини результатів - пошук ідеальних кандидатів
+
+    students = workAreaMatches;
+    /*
+    let set1 = techAndToolsMatches.filter((el) => workAreaMatches.includes(el));
+    
+    let set2 = set1.filter((el) => positionMatches.includes(el));
+    let set3 = set2.filter((el) => englishMatches.includes(el));
+    let set4 = set3.filter((el) => workExpMatches.includes(el));
+    let set5 = set4.filter((el) => educationMatches.includes(el));
+    let set6 = set5.filter((el) => regionMatches.includes(el));
+    let set7 = set6.filter((el) => salaryMatches.includes(el));
+    let set8 = set7.filter((el) => workplaceMatches.includes(el));
+    let idealCandiates = set8.filter((el) => cityMatches.includes(el));
+    */
+    
   }
-  //studentRegion
-  //studentCity
-  //studentEducation
-  //studentSalary
-  //studentWorkplace
+  return students;
+};
 
-  //studentTechAndTools
-  //studentEnglish
-  //studentWorkExp
-  //studentPosition
-  //studentWorkArea
-  return studentSql;
+const getMatchesByFilter = async function (filter, sql) {
+  let matches = [];
+  if (filter !== "" || filter !== null) matches = await connectionPromise(sql, "");
+  return matches;
 };
