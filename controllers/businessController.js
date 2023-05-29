e478af0d85b33877dec17574486a4478c13dfbcf
v-andrusenko <andrusenko.vp1@gmail.com>
2023-05-29 19:19:29 +0300

    update

--- controllers/businessController.js
+++ controllers/businessController.js
@@ -4,26 +4,6 @@ const filter = require("../models/filter.js");
 const dbHelper = new db();
 const filterHelper = new filter();
 
-exports.postBusiness = async function (request, response) {
-
-  const student = [
-    request.body.studentRegion,
-    request.body.studentCity,
-    request.body.studentEducation,
-    request.body.studentEnglish,
-    request.body.studentTechAndTools,
-    request.body.studentPosition,
-    request.body.studentWorkExp,
-    request.body.studentWorkArea,
-    request.body.studentSalary,
-    request.body.studentWorkplace,
-  ];
-
-  console.log(await connection(dbHelper.sqlGetAllStudentDetails, student));
-
-  response.send("OK");
-};
-
 exports.getResults = async function (req, res) {
   const students = await filterHelper.getResultsByFilters(req.query);
  
