744791f4820c87c9cfe0c760aec6228841bc2541
v-andrusenko <andrusenko.vp1@gmail.com>
2024-06-21 00:34:30 +0300

    u

--- controllers/candidateController.js
+++ controllers/candidateController.js
@@ -8,18 +8,11 @@ exports.postCandidates = async function (request, response) {
 
   await app.connection(dbHelper.sqlInsertCandidateDetails, Object.values(candidate));
 
-  const techAndToolsIds = request.body.candidateTechAndTools
-    .split(";")
-    .filter(function (el) {
-      return el != "";
-    })
-    .map(Number);
-
   techAndToolsSql = "";
-  techAndToolsIds.forEach((tech, index) => {
+  request.body.candidateTechAndTools.forEach((tech, index) => {
     if (index == 0) {
       techAndToolsSql += `INSERT INTO candidate_technology_tool(candidate_id, technology_tool_id) VALUES (LAST_INSERT_ID(), ${tech})`;
-    } else if (index == techAndToolsIds.length - 1 && index != 0) {
+    } else if (index == request.body.candidateTechAndTools.length - 1 && index != 0) {
       techAndToolsSql += `,(LAST_INSERT_ID(), ${tech});`;
     } else {
       techAndToolsSql += `,(LAST_INSERT_ID(), ${tech})`;
